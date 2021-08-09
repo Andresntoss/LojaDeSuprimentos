7a6c734c48e5c0ad4829d5539c77f5599490363e
Andresntoss <andre.sntos@hotmail.com>
2021-08-09 20:55:04 -0300

    Aula 09 exercicio 02

---
+++ src/main/kotlin/Loja/Fatura.kt
@@ -0,0 +1,14 @@
+package Loja
+
+class Fatura(val numeroDeItens:ArrayList<Item>) {
+
+    fun totalDaFatura(){
+        var contadora = 0
+        for(procura in numeroDeItens){
+            procura
+
+        }
+        return
+    }
+
+}
---
+++ src/main/kotlin/Loja/Item.kt
@@ -0,0 +1,9 @@
+package Loja
+
+class Item(
+    val numero:Int,
+    val descricao:String,
+    var quantidade:Int = 0,
+    var preco:Double = 0.0
+    ) {
+}
